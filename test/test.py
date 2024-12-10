89ff6446f69d9db10c67985ae8a74e65fe3a42a4
javi-yang <yang_chn@hotmail.com>
2024-12-10 14:49:00 +0800

    for file r/w

--- test/test.py
+++ test/test.py
@@ -1,8 +1,11 @@
-with open('/home/javi/python/trans/cmd_list.txt','r') as f:
-	for data in f.readlines():
-		data = data.strip('\n')
-		print(data)
-
+with open('/home/javi/leo_share/reg00.txt','r') as f:
+    for data in f.readlines():
+        #data = data.strip('\n')
+        if "i2cset" in data:
+            print(data)
+            with open('/home/javi/leo_share/test.txt','w') as r:
+                r.write(data)
+'''
 import datetime
 
 # 获取当前日期和时间
@@ -11,4 +14,8 @@ current_datetime = datetime.datetime.now()
 # 格式化当前日期和时间
 formatted_datetime = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
 
+with open('/home/javi/leo_share/test.txt','w') as f:
+	f.write(formatted_datetime+'\n')
+	#f.write('\n')
 print(formatted_datetime)
+'''
